04bb34a8641147f4ab09f0063fb08789c9ffb27f
Cesar Avina <cesaravina@gmail.com>
2017-11-16 14:46:27 -0800

    version 4 completed

--- watchandcode.js
+++ watchandcode.js
@@ -29,37 +29,100 @@
 // }
 
 // version 3
-var myComputer = {
-	operatingSystem: "mac",
-	screenSize: "15 inches",
-	purchaseYear: 2015
-}
+// var myComputer = {
+// 	operatingSystem: "mac",
+// 	screenSize: "15 inches",
+// 	purchaseYear: 2015
+// }
 
-var cesar = {
-	name: 'Cesar',
-	sayName: function() {
-		console.log(this.name);
-	}
-}
+// var cesar = {
+// 	name: 'Cesar',
+// 	sayName: function() {
+// 		console.log(this.name);
+// 	}
+// }
+
+// var todoList = {
+// 	todos: ['item 1', 'item 2', 'item 3'],
+// 	displayTodos: function() {
+// 		console.log('My Todos: ', this.todos)
+// 	},
+// 	addTodo: function(todo) {
+// 		this.todos.push(todo);
+// 		this.displayTodos();
+// 	},
+// 	changeTodo: function(position, newValue){
+// 		this.todos[position] = newValue,
+// 		this.displayTodos();
+// 	},
+// 	deleteTodo: function(position){
+// 		this.todos.splice(position, 1),
+// 		this.displayTodos();
+// 	}
+// };
+
+// version 4
 
 var todoList = {
-	todos: ['item 1', 'item 2', 'item 3'],
+	todos: [],
 	displayTodos: function() {
 		console.log('My Todos: ', this.todos)
 	},
-	addTodo: function(todo) {
-		this.todos.push(todo);
+	addTodo: function(todoText) {
+		this.todos.push({
+			todoText: todoText,
+			completed: false
+		});
 		this.displayTodos();
 	},
-	changeTodo: function(position, newValue){
-		this.todos[position] = newValue,
+	changeTodo: function(position, todoText){
+		this.todos[position].todoText = todoText,
 		this.displayTodos();
 	},
 	deleteTodo: function(position){
 		this.todos.splice(position, 1),
 		this.displayTodos();
+	},
+	toggleCompleted: function(position){
+		var todo = this.todos[position];
+		todo.completed = !todo.completed;
+		this.displayTodos();
 	}
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
