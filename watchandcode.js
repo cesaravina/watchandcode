674fdbca4edbd2716bbb4eb76c75959e009ce0bd
Cesar Avina <cesaravina@gmail.com>
2017-11-19 11:11:35 -0800

    version 5 completed

--- watchandcode.js
+++ watchandcode.js
@@ -63,10 +63,55 @@
 
 // version 4
 
+// var todoList = {
+// 	todos: [],
+// 	displayTodos: function() {
+// 		console.log('My Todos: ', this.todos)
+// 	},
+// 	addTodo: function(todoText) {
+// 		this.todos.push({
+// 			todoText: todoText,
+// 			completed: false
+// 		});
+// 		this.displayTodos();
+// 	},
+// 	changeTodo: function(position, todoText){
+// 		this.todos[position].todoText = todoText,
+// 		this.displayTodos();
+// 	},
+// 	deleteTodo: function(position){
+// 		this.todos.splice(position, 1),
+// 		this.displayTodos();
+// 	},
+// 	toggleCompleted: function(position){
+// 		var todo = this.todos[position];
+// 		todo.completed = !todo.completed;
+// 		this.displayTodos();
+// 	}
+// };
+
+// // version 5
+
+// var testArray = [2, 4, 6]
+// for (var i = 0; i < testArray.length; i++){
+// 	console.log(testArray[i])
+// }
+
 var todoList = {
 	todos: [],
 	displayTodos: function() {
-		console.log('My Todos: ', this.todos)
+		if (this.todos.length === 0){
+			console.log('Your todo list is empty!!!')
+		} else {
+			console.log('My Todos: ');
+			for (var i = 0; i < this.todos.length; i++){
+				if (this.todos[i].completed === true){
+					console.log('(x)', this.todos[i].todoText);
+				} else {
+					console.log('( )', this.todos[i].todoText);
+				}
+			}
+		}
 	},
 	addTodo: function(todoText) {
 		this.todos.push({
@@ -89,12 +134,6 @@ var todoList = {
 		this.displayTodos();
 	}
 };
-
-
-
-
-
-
 
 
 
