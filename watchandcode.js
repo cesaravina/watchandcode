8d3a2627d9474f851cf5cd97e20c30a40eca663b
Cesar Avina <cesaravina@gmail.com>
2017-12-01 20:48:41 -0800

    version 8 completed

--- watchandcode.js
+++ watchandcode.js
@@ -138,6 +138,86 @@
 
 // version 6
 
+// var todoList = {
+// 	todos: [],
+// 	displayTodos: function() {
+// 		debugger;
+// 		if (this.todos.length === 0){
+// 			console.log('Your todo list is empty!!!')
+// 		} else {
+// 			console.log('My Todos: ');
+// 			for (var i = 0; i < this.todos.length; i++){
+// 				if (this.todos[i].completed === true){
+// 					console.log('(x)', this.todos[i].todoText);
+// 				} else {
+// 					console.log('( )', this.todos[i].todoText);
+// 				}
+// 			}
+// 		}
+// 	},
+// 	addTodo: function(todoText) {
+// 		this.todos.push({
+// 			todoText: todoText,
+// 			completed: false
+// 		});
+// 		this.displayTodos();
+// 	},
+// 	changeTodo: function(position, todoText){
+// 		this.todos[position].todoText = todoText,
+// 		this.displayTodos();
+// 	},
+// 	deleteTodo: function(position){
+// 		this.todos.splice(position, 1),
+// 		this.displayTodos();
+// 	},
+// 	toggleCompleted: function(position){
+// 		var todo = this.todos[position];
+// 		todo.completed = !todo.completed;
+// 		this.displayTodos();
+// 	},
+// 	toggleAll: function() {
+// 		var totalTodos = this.todos.length
+// 		var completedTodos = 0;
+// 		// get number of completed todos.
+// 		for(var i = 0; i< totalTodos; i++){
+// 			if(this.todos[i].completed === true){
+// 				completedTodos++;
+// 			}
+// 		}
+// 		// if everything's true, make everything false.
+// 		if (completedTodos === totalTodos) {
+// 			for (var i = 0; i < totalTodos; i++) {
+// 				this.todos[i].completed = false;
+// 			}
+// 			// Case 2, otherwise, make everything true
+// 		}else {
+// 			for (var i = 0; i < totalTodos; i++){
+// 				this.todos[i].completed = true;
+// 			}
+// 		}
+// 		this.displayTodos();
+// 	}
+// };
+
+// // version 7
+// // Requirements:
+// // *There should be 'Display todos' & 'toggle all' buttons
+// // *Clicking "Display todos" should run todoList.displayTodos()
+// // *Clicking "Toggle all" should run todoList.toggleAll()
+
+// var displayTodosButton = document.getElementById("displayTodosButton");
+// var toggleAllButton = document.getElementById("toggleAllButton");
+
+// displayTodosButton.addEventListener('click', function(){
+// 	todoList.displayTodos();
+// });
+
+// toggleAllButton.addEventListener('click', function(){
+// 	todoList.toggleAll();
+// })
+
+// version 8
+
 var todoList = {
 	todos: [],
 	displayTodos: function() {
@@ -198,25 +278,48 @@ var todoList = {
 	}
 };
 
-// version 7
-// Requirements:
-// *There should be 'Display todos' & 'toggle all' buttons
-// *Clicking "Display todos" should run todoList.displayTodos()
-// *Clicking "Toggle all" should run todoList.toggleAll()
+// Old buttons and handlers before refactoring.
+// // var displayTodosButton = document.getElementById("displayTodosButton");
+// // var toggleAllButton = document.getElementById("toggleAllButton");
 
-var displayTodosButton = document.getElementById("displayTodosButton");
-var toggleAllButton = document.getElementById("toggleAllButton");
+// displayTodosButton.addEventListener('click', function(){
+// 	todoList.displayTodos();
+// });
 
-displayTodosButton.addEventListener('click', function(){
-	todoList.displayTodos();
-});
+// toggleAllButton.addEventListener('click', function(){
+// 	todoList.toggleAll();
+// });
 
-toggleAllButton.addEventListener('click', function(){
-	todoList.toggleAll();
-})
-
-
-
+var handlers = {
+	displayTodos: function(){
+		todoList.displayTodos();
+	},
+	addTodo: function() {
+		var addTodoTextInput = document.getElementById('addTodoTextInput');
+		todoList.addTodo(addTodoTextInput.value);
+		addTodoTextInput.value = ""
+	},
+	changeTodo: function() {
+		var changeTodoPositionInput = document.getElementById('changeTodoPositionInput');
+		var changeTodoTextInput = document.getElementById('changeTodoTextInput');
+		todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value)
+		changeTodoPositionInput.value = '';
+		changeTodoTextInput.value = '';
+	},
+	deleteTodo: function() {
+		var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
+		todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
+		deleteTodoPositionInput.value = ''
+	},
+	toggleCompleted: function() {
+		var toggleCompletedPositionInput = document.getElementById('toggleCompletedPositionInput');
+		todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
+		toggleCompletedPositionInput.value = ''
+	},
+	toggleAll: function(){
+		todoList.toggleAll();
+	}
+};
 
 
 
